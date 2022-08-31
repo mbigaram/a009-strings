dd7922fce2203aedfb584adcb998560b90aa741c
mbiga <mbigaram.mb@gmail.com>
2022-08-31 16:06:34 -0300

    Exercicio 3

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,8 +1,9 @@
 //Exercicio 1
 const nome = prompt("Qual o seu nome?")
-const prComida = prompt("Quais as 3 comidas que voce mais gosta?\nA primeira é?")
-const segComida = prompt("Qual a segunda?")
-const terComida = prompt("E a terceira?")
+
+let prComida = prompt("Quais as 3 comidas que voce mais gosta?\nA primeira é?")
+let segComida = prompt("Qual a segunda?")
+let terComida = prompt("E a terceira?")
 
 console.log ("Estas são as comidas favoritas de "+nome+".\n - "+prComida+"\n - "+segComida+"\n - "+terComida)
 
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,16 @@
 //Crie a const para a frase aqui
+const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\""
+
+console.log (frase)
+
+const umaCor = frase.replace("verde","rosa")
+
+const duasCores =umaCor.replace("azul","laranja")
+
+console.log (duasCores)
+console.log ("Verde = "+duasCores.includes("verde") , 
+"\nLaranja = "+duasCores.includes("laranja"))
+
+const fraseComMaiusculas = duasCores.replace("mas não deixe o gato sair","MAS NÃO DEIXE O GATO SAIR")
+
+console.log (fraseComMaiusculas)
