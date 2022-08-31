68002b60e16b6615b8dd76606e2f3ced7d8e56d1
mbiga <mbigaram.mb@gmail.com>
2022-08-31 12:43:35 -0300

    Exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+//Exercicio 1
+const nome = prompt("Qual o seu nome?")
+const prComida = prompt("Quais as 3 comidas que voce mais gosta?\nA primeira é?")
+const segComida = prompt("Qual a segunda?")
+const terComida = prompt("E a terceira?")
+
+console.log ("Estas são as comidas favoritas de "+nome+".\n - "+prComida+"\n - "+segComida+"\n - "+terComida)
+
